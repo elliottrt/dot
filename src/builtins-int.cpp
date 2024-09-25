4e85df581d93ae45def873f861ede7a2ff36d9ef
elliottrt <reedelliott1@icloud.com>
2024-09-25 14:00:29 -0400

    if and while

--- src/builtins-int.cpp
+++ src/builtins-int.cpp
@@ -47,10 +47,64 @@ DOT_DECLARE(dot_mod) {
 	return self;
 }
 
+DOT_DECLARE(dot_if) {
+	integer_type &cond = self->get_int(loc);
+	function_type &then = arg->get_function(loc);
+
+	bool is_true = cond != 0;
+
+	if (is_true) {
+		object_ptr then_self = std::make_shared<object>();
+		object_ptr then_arg = std::make_shared<object>();
+
+		then.bind_to(then_self);
+		then(then_arg, loc);
+	}
+
+	return self;
+}
+
+DOT_DECLARE(dot_else) {
+	integer_type &cond = self->get_int(loc);
+	function_type &then = arg->get_function(loc);
+
+	bool is_true = cond != 0;
+
+	if (is_true == false) {
+		object_ptr then_self = std::make_shared<object>();
+		object_ptr then_arg = std::make_shared<object>();
+
+		then.bind_to(then_self);
+		then(then_arg, loc);
+	}
+
+	return self;
+}
+
+DOT_DECLARE(dot_while) {
+	integer_type &cond = self->get_int(loc);
+	function_type &then = arg->get_function(loc);
+
+	// because self is considered the parent here, changing value in the loop
+	// changes self.value
+	object_ptr then_arg = std::make_shared<object>();
+
+	// note: cond is updated because it's a reference, so basically a pointer
+	while (cond != 0) {
+		then.bind_to(self);
+		then(then_arg, loc);
+	}
+
+	return self;
+}
+
 builtins::builtin_list_type builtins::INTEGER_FUNCTIONS = {
 	{"add", dot_add},
 	{"sub", dot_sub},
 	{"mul", dot_mul},
 	{"div", dot_div},
-	{"mod", dot_mod}
+	{"mod", dot_mod},
+	{"if", dot_if},
+	{"else", dot_else},
+	{"while", dot_while}
 };
